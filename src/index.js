5b319a405f1fee5d178a748dc7b4a6c904f23096
Olya Akinshyna <olga.akinshyna@gmail.com>
2017-07-14 13:31:58 +0300

    styles added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import ChatComponent from './components/Chat'
+import './assets/styles/style.scss';
 
 render(
     <ChatComponent />,
